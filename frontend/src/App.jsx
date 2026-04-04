e86a17303baa829a79ce35245c1abf5e5b431abb
Dev Sanghavi <you@example.com>
2026-04-04 09:09:49 +0530

    Landing Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { Routes, Route, Navigate } from 'react-router-dom'
+import LandingPage from './pages/LandingPage.jsx'
 
 function App() {
-
   return (
-    <>
-  <div className='bg-red-200'>hi</div>
-    </>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   )
 }
 
